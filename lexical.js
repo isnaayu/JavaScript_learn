6d95070c09ed0d1e7355de3095173d968959b347
isnaayu_m <isnaayu2@gmail.com>
2024-01-04 08:42:28 +0700

    [FEATURE] - lexical

--- lexical.js
+++ lexical.js
@@ -1,38 +1,38 @@
-let a = 10
+let a = 10;
 const foo = () => {
-    console.log(`get variable outer a ${a}`)
-    let b = 10
-    const bar = () => {
-        console.log(`get variable b inner ${b}`)
-    }
+  console.log(`get variable outer a ${a}`);
+  let b = 10;
+  const bar = () => {
+    console.log(`get variable b inner ${b}`);
+  };
 
-    bar()
-}
+  bar();
+};
 
-foo()
+foo();
 // bar() -> Error (not global function)
 
-let counter = 1
+let counter = 1;
 const increment = () => {
-    let angka = 4
-    angka++
-    return angka
-    // return counter += 1
+  let angka = 4;
+  angka++;
+  return angka;
+  // return counter += 1
+};
+
+console.log(increment());
+console.log(increment());
+
+function tampilkanNama(name, func) {
+  if (typeof func === "function") {
+    name = func.call(this, name);
+  }
+  return name;
 }
 
-console.log(increment())
-console.log(increment())
-
-function tampilkanNama(name, func){
-    if(typeof func === 'function'){
-        name = func.call(this, name)
-    }
-    return name
+function capitalization(name) {
+  return name.toUpperCase();
 }
 
-function capitalization(name){
-    return name.toUpperCase()
-}
-
-console.log(tampilkanNama("Andi"))
-console.log(tampilkanNama("Adam", capitalization))
+console.log(tampilkanNama("Andi"));
+console.log(tampilkanNama("Adam", capitalization));
